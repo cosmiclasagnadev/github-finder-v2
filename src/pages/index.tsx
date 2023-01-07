a5c30e65247ba522d727b604446eee5f3a01251f
Allen Ponce de Leon <aoponcedeleon@up.edu.ph>
2023-01-07 12:16:58 +0800

    Fixed paginate and refactored fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,71 +1,81 @@
 import {ActionIcon, Card, Container, LoadingOverlay, SimpleGrid, Skeleton, Text, TextInput, useMantineTheme} from "@mantine/core";
 import {IconSearch} from "@tabler/icons";
-import React from "react";
+import React, {useEffect} from "react";
 import SearchDisplay from "../components/SearchDisplay";
 import axios from "axios";
 import {UserResult} from "../types/users";
 import {showNotification} from "@mantine/notifications";
 import CardLoader from "../components/CardLoader";
 
+export const fetchConfig = {
+  headers: {
+    authorization: `token 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`
+  }
+}
+
 export default function IndexPage() {
   const theme = useMantineTheme();
   const [searchQuery, setSearch] = React.useState('');
+  const [submittedString, setSubmittedString] = React.useState('');
   const [activePage, setPage] = React.useState(1);
   const [results, setResults] = React.useState<Array<UserResult>>([]);
   const [loading, setLoading] = React.useState<boolean>(false);
   const [totalResults, setTotalresults] = React.useState<number>(0);
 
 
-
-  const handleQuery = async () => {
-    setLoading(true);
-    try {
-      const response: any = await axios.get("https://api.github.com/search/users?q=" + searchQuery + `&page=${activePage}`);
-      const users = response.data.items;
-      console.log(response);
-      setResults(users);
-      setTotalresults(response.data.total_count);
-    } catch (error: any) {
-      console.log(error);
-      if (error.response.status === 422) {
-        showNotification({
-          title: "Error",
-          message: "Please enter a valid search query",
-          color: "red",
-        })
-      } else {
-        showNotification({
-          title: "Error",
-          message: "Something went wrong. Please try again later",
-          color: "red",
-        })
+  useEffect(() => {
+    async function updateDataOnPaginate() {
+      try {
+        if (submittedString.length > 0) {
+          const response: any = await axios.get("https://api.github.com/search/users?q=" + submittedString + `&page=${activePage}`, fetchConfig);
+          const users = response.data.items;
+          setResults(current => users);
+          setTotalresults(response.data.total_count);
+        } else {
+          console.log('Initial Load 🚀')
+        }
+      } catch (error: any) {
+        if (error.response.status === 422) {
+          showNotification({
+            title: "Error",
+            message: "Please enter a valid search query",
+            color: "red",
+          })
+        } else {
+          showNotification({
+            title: "Error",
+            message: "Something went wrong. Please try again later",
+            color: "red",
+          })
+        }
       }
     }
+
+    updateDataOnPaginate();
+
+  }, [activePage, submittedString])
+
+
+  const handleSubmit = () => {
+    setLoading(true);
+    setSubmittedString(searchQuery);
     setLoading(false);
   }
 
   const handlePaginate = async (page: number) => {
     console.log('Getting page: ', page);
-    try {
-      setPage(page);
-      const response: any = await axios.get("https://api.github.com/search/users?q=" + searchQuery + `&page=${activePage}`);
-      const users = response.data.items;
-      setResults(current => users);
-    } catch (error: any) {
-      showNotification({
-        title: "Error",
-        message: "Something went wrong. Please try again later",
-        color: "red",
-      })
-    }
+    setLoading(true);
+    setPage(page);
+    setLoading(false);
   }
+
   return (
     <Container size="xl">
       <TextInput
         size="md"
         mb={18}
         rightSection={
-          <ActionIcon size={32} color={theme.primaryColor} variant="filled" onClick={handleQuery}>
+          <ActionIcon size={32} color={theme.primaryColor} variant="filled" onClick={handleSubmit}>
             <IconSearch size={18} stroke={1.5} />
           </ActionIcon>
         }
